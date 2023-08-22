61056a8cc8a56ad7c7c2c22b5274adfb1f1f89d7
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-08-21 17:49:37 -0700

    commit tree 635091d4bd79bfdf2d198288be64843f3d618e6b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: :true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    clickbaity_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    if clickbaity_titles.include?(:title) || title == "True Facts"
+      errors.add(:title, "Your title needs to be more clickbaity!")
+    end
+  end
+
 end
